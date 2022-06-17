1d8db9e8ef77406983945fe4b8c1bca8ae3b3d83
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-17 15:37:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("\a")
+
+x = 5;
+
+if x != 6 :
+  print("x is 5!! ")
+
+if x == 5 :
+  print("x is 5!! ")
+
+if x <= 5 :
+  print("x is 5 or less ")
+
+if x != 4 :
+  print("x is not 4 but could be anything greater/smaller")
+
+if x < 6 and x > 4 :
+  print("x is >4 & <6")
